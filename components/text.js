829f9ba4262c4350c039f15ea184781206e07ce5
steinhakasevs <stein.hakase.vs@gmail.com>
2019-09-01 22:17:19 -0600

    finished introductory card component

--- components/text.js
+++ components/text.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-export default ({ children, style, fontFamily = 'regular' }) => (
-  <Text style={[ ...style, { fontFamily } ]}>
-    {
-      children
-    }
-  </Text>
-)
+export default ({ children, style = null, fontFamily = 'regular' }) => {
+  console.log(style)
+  if (style) {
+    return (
+      <Text style={[...style, { fontFamily }]}>
+        {
+          children
+        }
+      </Text>
+    )
+  } else {
+    return (
+      <Text style={[{ fontFamily }]}>
+        {
+          children
+        }
+      </Text>
+    )
+  }
+}
